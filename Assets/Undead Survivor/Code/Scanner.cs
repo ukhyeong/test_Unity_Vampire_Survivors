c92cbc232a9d2ef819aa2464eaa180e7f443ab49
ukhyeong <ukh0702@gmail.com>
2024-10-30 15:46:13 +0900

    asdf

--- Assets/Undead Survivor/Code/Scanner.cs
+++ Assets/Undead Survivor/Code/Scanner.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Scanner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float scanRange;
+    public LayerMask targetLayer;
+    public RaycastHit2D[] targets;
+    public Transform nearestTarget;
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate() 
     {
-        
-    }
+        this.targets = Physics2D.CircleCastAll(this.transform.position, this.scanRange, Vector2.zero, 0, this.targetLayer);
+    } // FixedUpdate
+
+    Transform GetNearest() 
+    {
+        Transform result = null;
+        float diff = 100;
+
+        foreach (RaycastHit2D target in targets) {
+            Vector3 myPos = this.transform.position;
+            Vector3 targetPos = target.transform.position;
+
+            // Vetor3 사이의 거리를 구함
+            float curDiff = Vector3.Distance(myPos, targetPos);
+
+            if (curDiff < diff) {
+
+            } // if
+
+        } // foreach, inhanced for 과 유사
+
+        return result;
+    } // GetNearest
+
+
 } // end class
